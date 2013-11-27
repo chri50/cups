466dbbedd6836e9a32b203d79c005cbc795a5a58
Didier Raboud <odyx@debian.org>
2013-11-27 17:19:20 +0100

    log-debug-history-nearly-unlimited.patch
    No DEP3 Subject or Description header found
    
    Gbp-Pq: log-debug-history-nearly-unlimited.patch.

--- scheduler/conf.h
+++ scheduler/conf.h
@@ -167,7 +167,7 @@ VAR int			ClassifyOverride	VALUE(0),
 					/* Allow overrides? */
 			ConfigFilePerm		VALUE(0640),
 					/* Permissions for config files */
-			LogDebugHistory		VALUE(200),
+			LogDebugHistory		VALUE(99999),
 					/* Amount of automatic debug history */
 			FatalErrors		VALUE(CUPSD_FATAL_CONFIG),
 					/* Which errors are fatal? */
