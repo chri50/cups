4ad7b92a0a644b4e1f946f636658cea3835c80a5
Ryan Tandy <rtandy@sd63.bc.ca>
2014-03-04 13:07:24 -0800

    Handle server terminating the connection
    Gbp-Pq: handle-server-terminating-connection.patch.

--- cups/request.c
+++ cups/request.c
@@ -1004,6 +1004,26 @@ _cupsConnect(void)
       httpClose(cg->http);
       cg->http = NULL;
     }
+    else
+    {
+     /*
+      * Same server, see if the connection is still established...
+      */
+
+      char     ch;                     /* Connection check byte */
+      ssize_t  n;                      /* Number of bytes */
+
+      if ((n = recv(cg->http->fd, &ch, 1, MSG_PEEK | MSG_DONTWAIT)) == 0 ||
+          (n < 0 && errno != EWOULDBLOCK))
+      {
+       /*
+        * Nope, close the connection...
+        */
+
+	httpClose(cg->http);
+	cg->http = NULL;
+      }
+    }
   }
 
  /*
