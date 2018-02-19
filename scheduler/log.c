0b95f1e3047ebe699a4ce056173102848ffc0f38
Chris Coulson <chris.coulson@canonical.com>
2018-02-19 17:37:01 +0000

    Make log files readable to group "adm", if present.
    Gbp-Pq: logfiles_adm_readable.patch.

--- scheduler/log.c
+++ scheduler/log.c
@@ -26,6 +26,7 @@
 #  include <systemd/sd-journal.h>
 #endif /* HAVE_ASL_H */
 #include <syslog.h>
+#include <grp.h>
 
 
 /*
@@ -115,6 +116,7 @@ cupsdCheckLogFile(cups_file_t **lf,	/* IO - Log file */
 		filename[1024],		/* Formatted log filename */
 		*ptr;			/* Pointer into filename */
   const char	*logptr;		/* Pointer into log filename */
+  struct group  *loggrp;		/* Group entry of log filename */
 
 
  /*
@@ -133,6 +135,11 @@ cupsdCheckLogFile(cups_file_t **lf,	/* IO - Log file */
     *lf = LogStderr;
     return (1);
   }
+
+ /*
+  * Use adm group if possible, fall back to Group
+  */
+ loggrp = getgrnam("adm");
 
  /*
   * Format the filename as needed...
@@ -255,7 +262,7 @@ cupsdCheckLogFile(cups_file_t **lf,	/* IO - Log file */
       * Change ownership and permissions of non-device logs...
       */
 
-      fchown(cupsFileNumber(*lf), RunUser, Group);
+      fchown(cupsFileNumber(*lf), RunUser, loggrp ? loggrp->gr_gid : Group);
       fchmod(cupsFileNumber(*lf), LogFilePerm);
     }
   }
@@ -306,7 +313,7 @@ cupsdCheckLogFile(cups_file_t **lf,	/* IO - Log file */
     * Change ownership and permissions of non-device logs...
     */
 
-    fchown(cupsFileNumber(*lf), RunUser, Group);
+    fchown(cupsFileNumber(*lf), RunUser, loggrp ? loggrp->gr_gid : Group);
     fchmod(cupsFileNumber(*lf), LogFilePerm);
   }
 
