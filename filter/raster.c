0067b317afee6a13e97ad07e27e79d71493f6e87
Marc Deslauriers <marc.deslauriers@ubuntu.com>
2018-06-22 13:53:17 -0400

    fix buffer overflow in cupsRasterReadPixels
    Gbp-Pq: CVE-2014-9679.patch.

--- filter/raster.c
+++ filter/raster.c
@@ -281,7 +281,10 @@ cupsRasterReadHeader(
   */
 
   if (!cups_raster_read_header(r))
+  {
+    memset(h, 0, sizeof(cups_page_header_t));
     return (0);
+  }
 
  /*
   * Copy the header to the user-supplied buffer...
@@ -310,7 +313,10 @@ cupsRasterReadHeader2(
   */
 
   if (!cups_raster_read_header(r))
+  {
+    memset(h, 0, sizeof(cups_page_header2_t));
     return (0);
+  }
 
  /*
   * Copy the header to the user-supplied buffer...
@@ -988,7 +994,7 @@ cups_raster_read_header(
 
   cups_raster_update(r);
 
-  return (r->header.cupsBytesPerLine != 0 && r->header.cupsHeight != 0);
+  return (r->header.cupsBytesPerLine != 0 && r->header.cupsHeight != 0 && (r->header.cupsBytesPerLine % r->bpp) == 0);
 }
 
 
