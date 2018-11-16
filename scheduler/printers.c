14b6f01f3fe68184f5867efac26681e2af2a89cc
Marc Deslauriers <marc.deslauriers@ubuntu.com>
2018-11-16 14:07:12 -0500

    Color management per queue using colord.
    Gbp-Pq: color-management-extension.patch.

--- scheduler/printers.c
+++ scheduler/printers.c
@@ -94,12 +94,13 @@ cupsdAddPrinter(const char *name)	/* I - Name of printer */
   cupsdSetString(&p->uuid, _httpAssembleUUID(ServerName, RemotePort, name, 0,
                                              uuid, sizeof(uuid)));
   cupsdSetDeviceURI(p, "file:///dev/null");
-
-  p->state      = IPP_PRINTER_STOPPED;
-  p->state_time = time(NULL);
-  p->accepting  = 0;
-  p->shared     = DefaultShared;
-  p->filetype   = mimeAddType(MimeDatabase, "printer", name);
+  cupsdSetString(&p->ppd_timestamp, "*");
+  p->state         = IPP_PRINTER_STOPPED;
+  p->state_time    = time(NULL);
+  p->accepting     = 0;
+  p->color_managed = 1;
+  p->shared        = DefaultShared;
+  p->filetype      = mimeAddType(MimeDatabase, "printer", name);
 
   cupsdSetString(&p->job_sheets[0], "none");
   cupsdSetString(&p->job_sheets[1], "none");
@@ -753,6 +754,7 @@ cupsdDeletePrinter(
   cupsdClearString(&p->job_sheets[0]);
   cupsdClearString(&p->job_sheets[1]);
   cupsdClearString(&p->device_uri);
+  cupsdClearString(&p->ppd_timestamp);
   cupsdClearString(&p->sanitized_device_uri);
   cupsdClearString(&p->port_monitor);
   cupsdClearString(&p->op_policy);
@@ -952,6 +954,11 @@ cupsdLoadAllPrinters(void)
       if (value)
 	cupsdSetString(&p->make_model, value);
     }
+    else if (!_cups_strcasecmp(line, "PPDTimeStamp"))
+    {
+      if (value)
+	cupsdSetString(&p->ppd_timestamp, value);
+    }
     else if (!_cups_strcasecmp(line, "Location"))
     {
       if (value)
@@ -1105,6 +1112,26 @@ cupsdLoadAllPrinters(void)
         	!_cups_strcasecmp(value, "off") ||
         	!_cups_strcasecmp(value, "false")))
         p->shared = 0;
+      else
+	cupsdLogMessage(CUPSD_LOG_ERROR,
+	                "Syntax error on line %d of printers.conf.", linenum);
+    }
+    else if (!_cups_strcasecmp(line, "ColorManaged"))
+    {
+     /*
+      * Set the initial color-managed state...
+      */
+
+      if (value &&
+          (!_cups_strcasecmp(value, "yes") ||
+           !_cups_strcasecmp(value, "on") ||
+           !_cups_strcasecmp(value, "true")))
+        p->color_managed = 1;
+      else if (value &&
+               (!_cups_strcasecmp(value, "no") ||
+        	!_cups_strcasecmp(value, "off") ||
+        	!_cups_strcasecmp(value, "false")))
+        p->color_managed = 0;
       else
 	cupsdLogMessage(CUPSD_LOG_ERROR,
 	                "Syntax error on line %d of printers.conf.", linenum);
@@ -1415,6 +1442,9 @@ cupsdSaveAllPrinters(void)
 
     cupsFilePutConf(fp, "DeviceURI", printer->device_uri);
 
+    if (printer->ppd_timestamp)
+      cupsFilePutConf(fp, "PPDTimeStamp", printer->ppd_timestamp);
+
     if (printer->port_monitor)
       cupsFilePutConf(fp, "PortMonitor", printer->port_monitor);
 
@@ -1447,6 +1477,11 @@ cupsdSaveAllPrinters(void)
       cupsFilePuts(fp, "Shared Yes\n");
     else
       cupsFilePuts(fp, "Shared No\n");
+
+    if (printer->color_managed)
+      cupsFilePuts(fp, "ColorManaged Yes\n");
+    else
+      cupsFilePuts(fp, "ColorManaged No\n");
 
     snprintf(value, sizeof(value), "%s %s", printer->job_sheets[0],
              printer->job_sheets[1]);
@@ -2089,6 +2124,8 @@ cupsdSetPrinterAttrs(cupsd_printer_t *p)/* I - Printer to setup */
                NULL, p->info ? p->info : "");
   ippAddString(p->attrs, IPP_TAG_PRINTER, IPP_TAG_URI, "printer-uuid", NULL,
 	       p->uuid);
+  ippAddString(p->attrs, IPP_TAG_PRINTER, IPP_TAG_TEXT,
+               "ppd-timestamp", NULL, p->ppd_timestamp ? p->ppd_timestamp : "*");
 
   if (cupsArrayCount(p->users) > 0)
   {
@@ -3027,6 +3064,7 @@ cupsdWritePrintcap(void)
 			     "\t\t<%s/>\n"
 			     "\t\t<key>printer-location</key>\n"
 			     "\t\t<string>", p->accepting ? "true" : "false");
+
 	  write_xml_string(fp, p->location);
 	  cupsFilePuts(fp, "</string>\n"
 			   "\t\t<key>printer-make-and-model</key>\n"
