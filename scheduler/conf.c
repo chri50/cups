2e74184eca283995a30bef48961172a669d1c9a2
Till Kamppeter <till.kamppeter@gmail.com>
2019-08-05 11:40:10 +0200

    set-dirty-clean-interval-to-zero.patch
    No DEP3 Subject or Description header found
    
    Gbp-Pq: set-dirty-clean-interval-to-zero.patch.

--- scheduler/conf.c
+++ scheduler/conf.c
@@ -709,7 +709,7 @@ cupsdReadConfiguration(void)
   CreateSelfSignedCerts    = TRUE;
   DefaultEncryption        = HTTP_ENCRYPT_REQUIRED;
 #endif /* HAVE_SSL */
-  DirtyCleanInterval       = DEFAULT_KEEPALIVE;
+  DirtyCleanInterval       = 0;
   JobKillDelay             = DEFAULT_TIMEOUT;
   JobRetryLimit            = 5;
   JobRetryInterval         = 300;
