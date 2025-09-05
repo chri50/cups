c0e3ec8b786b2ef03ff3b59f4a7329584541d037
Eduardo Barretto <eduardo.barretto@canonical.com>
2025-09-05 17:58:36 +0200

    Debian: Build mantohtml with the build architecture compiler
    Gbp-Pq: 0013-Debian-Build-mantohtml-with-the-build-architecture-c.patch.

--- man/mantohtml.c
+++ man/mantohtml.c
@@ -11,8 +11,10 @@
  * Include necessary headers.
  */
 
-#include <cups/string-private.h>
-#include <cups/array-private.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 
@@ -811,7 +813,8 @@ main(int  argc,				/* I - Number of command-line args */
         * Anchor for HTML output...
         */
 
-        strlcpy(anchor, line + 4, sizeof(anchor));
+        strncpy(anchor, line + 4, sizeof(anchor) - 1);
+        anchor[sizeof(anchor) - 1] = '\0';
       }
       else if (strncmp(line, ".\\\"", 3))
       {
@@ -944,7 +947,8 @@ html_alternate(const char *s,		/* I - String */
 		manfile[1024],		// Man page filename
 		manurl[1024];		// Man page URL
 
-        strlcpy(name, s, sizeof(name));
+        strncpy(name, s, sizeof(name) - 1);
+        name[sizeof(name) - 1] = '\0';
         if ((size_t)(end - s) < sizeof(name))
           name[end - s] = '\0';
 
